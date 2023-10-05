874700fa8fe85e921af6884ae95f1aebabb44f9b
Irem Gülser <iremgulser@std.iyte.edu.tr>
2023-10-05 15:15:38 +0300

    Add files via upload

---
+++ ParcelationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using tst.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace tst
+{
+
+    public class ParcelationContext : DbContext
+    {
+        public ParcelationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Parcelation> Parcelation { get; set; }
+    
+
+    }
+}
+
